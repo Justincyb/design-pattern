fa38693c41f37a0f1f6c890f14d26c37492c0dc4
ybcheng <ybcheng@shcepp.com>
2018-06-26 17:09:01 +0800

    新增双检查单例 线程安全

---
+++ src/method/singleton/DoubleCheckSingelton.java
@@ -0,0 +1,55 @@
+package method.singleton;
+
+/**
+ * @Author Justin
+ * @Time 2018/6/26 16:17
+ * @Description 双重检查单例（线程安全，效率高 SafeSingelton升级版）
+ * 双重检查 在synchronized基础上做的改进,我们没必要对整个方法同步，其实我们需要同步的仅仅是创建实例这个动作
+ */
+public class DoubleCheckSingelton {
+
+    // 私有构造，客户端无法自己创建Singleton类实例
+    private DoubleCheckSingelton() {
+    }
+
+    private volatile static DoubleCheckSingelton doubleCheckSingelton;
+
+    // 全局唯一访问入口
+    public static DoubleCheckSingelton getDoubleCheckSingelton() {
+        if (doubleCheckSingelton == null) {
+            synchronized (DoubleCheckSingelton.class) {
+                if (doubleCheckSingelton == null) {
+                    doubleCheckSingelton = new DoubleCheckSingelton();
+                }
+            }
+        }
+        return doubleCheckSingelton;
+    }
+
+    public static void main(String[] args) {
+        // 调用端以这种方式去调用，在其他调用类通过new该实例是不允许的
+        long st = System.currentTimeMillis();
+        Thread t1 = new Thread(() -> {
+            DoubleCheckSingelton doubleCheckSingelton = DoubleCheckSingelton.getDoubleCheckSingelton();
+            System.out.println("t1:" + doubleCheckSingelton);
+        }
+        );
+        Thread t2 = new Thread(() -> {
+            DoubleCheckSingelton doubleCheckSingelton = DoubleCheckSingelton.getDoubleCheckSingelton();
+            System.out.println("t2:" + doubleCheckSingelton);
+        }
+        );
+        Thread t3 = new Thread(() -> {
+            DoubleCheckSingelton doubleCheckSingelton = DoubleCheckSingelton.getDoubleCheckSingelton();
+            System.out.println("t3:" + doubleCheckSingelton);
+        }
+        );
+        t1.start();
+        t2.start();
+        t3.start();
+        System.out.println("耗时：" + (System.currentTimeMillis() - st));
+
+    }
+
+
+}
---
+++ src/method/singleton/SafeSingelton.java
@@ -0,0 +1,46 @@
+package method.singleton;
+
+/**
+ * @Author Justin
+ * @Time 2018/6/26 15:26
+ * @Description 线程安全的单例模式（UnSafeSingleton改进版）
+ * UnSafeSingleton虽然延迟加载了，但是线程不安全，为了解决这个问题，我们引入synchronized进行同步实现安全
+ * 缺点：synchronized虽然保证了同步，但是限制了线程访问的效率
+ */
+public class SafeSingelton {
+
+    // 私有构造，客户端无法自己创建Singleton类实例
+    private SafeSingelton() {
+    }
+
+    static SafeSingelton safeSingelton;
+
+    // 全局唯一访问入口
+    public static synchronized SafeSingelton getSafeSingelton() {
+        if (safeSingelton == null) {
+            safeSingelton = new SafeSingelton();
+        }
+        return safeSingelton;
+    }
+
+    public static void main(String[] args) {
+        // 调用端以这种方式去调用，在其他调用类通过new该实例是不允许的
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 2; i++) {
+                SafeSingelton safeSingelton = SafeSingelton.getSafeSingelton();
+                System.out.println("t1:" + safeSingelton);
+            }
+        }
+        );
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 2; i++) {
+                SafeSingelton safeSingelton = SafeSingelton.getSafeSingelton();
+                System.out.println("t2:" + safeSingelton);
+            }
+        }
+        );
+        t1.start();
+        t2.start();
+    }
+
+}
